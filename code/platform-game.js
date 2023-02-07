e72c9e7892abd2f11d091972f1c8cafff041df10
theEmperorofDaiViet <Khiet.To.Development@gmail.com>
2023-02-07 15:57:52 +0700

    Exercise 2

--- code/platform-game.js
+++ code/platform-game.js
@@ -301,6 +301,10 @@ function trackKeys(keys) {
     }
     window.addEventListener("keydown", track);
     window.addEventListener("keyup", track);
+    down.unregister = () => {
+        window.removeEventListener("keydown", track);
+        window.removeEventListener("keyup", track);
+    };
     return down;
 }
   
@@ -324,8 +328,30 @@ function runLevel(level, Display) {
     let display = new Display(div, level);
     let state = State.start(level);
     let ending = 1;
+    let running = "yes";
+    
     return new Promise(resolve => {
-        runAnimation(time => {
+        function escHandler(event) {
+            if (event.key != "Escape") return;
+            event.preventDefault();
+            if (running == "no") {
+                running = "yes";
+                runAnimation(frame);
+            } else if (running == "yes") {
+                running = "pausing";
+            } else {
+                running = "yes";
+            }
+        }
+        window.addEventListener("keydown", escHandler);
+        let arrowKeys = trackKeys(["ArrowLeft", "ArrowRight", "ArrowUp"]);
+  
+        function frame(time) {
+            if (running == "pausing") {
+                running = "no";
+                return false;
+            }
+    
             state = state.update(time, arrowKeys);
             display.syncState(state);
             if (state.status == "playing") {
@@ -335,10 +361,13 @@ function runLevel(level, Display) {
                 return true;
             } else {
                 display.clear();
+                window.removeEventListener("keydown", escHandler);
+                arrowKeys.unregister();
                 resolve(state.status);
                 return false;
             }
-        });
+        }
+        runAnimation(frame);
     });
 }
 
